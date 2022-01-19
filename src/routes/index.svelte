3e227fe3e2a677556ca82ba1851d78d8847bd50b
braedenf <braedenleefoster@gmail.com>
2022-01-20 11:31:07 +1300

    Created base project demo

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,14 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<a-scene
+	vr-mode-ui="enabled: false"
+	embedded
+	arjs="sourceType: webcam; sourceWidth: 1280; sourceHeight: 960; displayWidth: 1280; displayHeight: 960; debugUIEnabled: false; "
+>
+	<a-camera gps-camera rotation-reader />
+	<a-entity
+		gltf-model="/assets/magnemite/scene.gltf"
+		rotation="0 180 0"
+		scale="0.15 0.15 0.15"
+		gps-entity-place="longitude: -36.8676046; latitude: 174.7270973"
+		animation-mixer
+	/>
+</a-scene>
